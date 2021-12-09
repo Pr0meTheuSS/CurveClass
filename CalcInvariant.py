8dafe47e8d2c4ae4b4be2e13af61a0070df55b4a
Юрий Юрьевич Олимпиев <y.olimpiev@g.nsu.ru>
2021-12-09 23:27:10 +0700

    First version of CalcInvariant module

--- CalcInvariant.py
+++ CalcInvariant.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class ECurveClassType(Enum):
     Ellipse
+    ImagineEllipse
     Hyperbola
     Parabola
     CrossedLines
@@ -21,16 +22,36 @@ class CalcInvariant():
     """
     Constructor
     """
-    self.CoefficientsMatrix = coeffMatrix
+        self.CoefficientsMatrix = coeffMatrix
     
 
     def GetInvariant():
     """
     Calculates all invariants and gets result as ECurveClassType variable 
     """
-    s = self.CoefficientsMatrix[0][0] + self.CoefficientsMatrix[1][1] 
-    # Calc matrix det range == 3
-    largeDelta = np.linalg.det(self.CoefficientsMatrix)
-    # Calc matrix det range == 2
-    smallDelta = np.linalg.det(self.CoefficientsMatrix.reshape((2, 2)))
-    if 
+        s = self.CoefficientsMatrix[0][0] + self.CoefficientsMatrix[1][1] 
+        # Calc matrix det range == 3
+        largeDelta = np.linalg.det(self.CoefficientsMatrix)
+        # Calc matrix det range == 2
+        smallDelta = np.linalg.det(self.CoefficientsMatrix.reshape((2, 2)))
+        # Chose compose of invariants to detect CurveType    
+        if smallDelta == 0:
+            if largeDelta != 0:
+                self.InvariantType = Parabola
+            else:
+                self.InvariantType = ParallelLines
+        elif smallDelta < 0:
+            if largeDelta != 0: 
+                self.InvariantType = Hyperbola
+            else:
+                self.InvariantType = CrossedLines
+        elif smallDelta > 0:
+            if largeDelta != 0:
+                if s * largeDelta < 0:
+                    self.InvariantType = Ellipse
+                else:
+                    self.InvariantType = ImagineEllipse
+            else:
+                self.InvariantType = Point
+
+
